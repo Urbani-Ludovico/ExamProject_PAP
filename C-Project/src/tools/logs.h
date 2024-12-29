0b56d55e8a0f7a96228f165bb9205a288b440306
Ludovico Urbani <lu.urbani@outlook.com>
2024-12-29 17:45:36 +0100

    Added logs macro

---
+++ C-Project/src/tools/logs.h
@@ -0,0 +1,11 @@
+//
+// Ludovico Urbani <SM3201372>
+//
+
+#ifndef LOGS_H
+#define LOGS_H
+
+#define LOG_ERROR(title, content) "\n\033[1;31mERROR: " title "\n\033[0;31m" content "\033[31m\n"
+#define LOG_STEP(string) string "...\n"
+
+#endif

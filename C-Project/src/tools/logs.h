5606ab37139e34f9b85fd62545f2397fcb323a60
Ludovico Urbani <lu.urbani@outlook.com>
2024-12-30 14:06:47 +0100

    Fixed error color

--- C-Project/src/tools/logs.h
+++ C-Project/src/tools/logs.h
@@ -5,7 +5,7 @@
 #ifndef LOGS_H
 #define LOGS_H
 
-#define LOG_ERROR(title, content) "\n\033[1;31mERROR: " title "\n\033[0;31m" content "\033[31m\n"
+#define LOG_ERROR(title, content) "\n\033[1;31mERROR: " title "\n\033[0;31m" content "\033[0m\n\n"
 #define LOG_STEP(string) string "...\n"
 
 #endif

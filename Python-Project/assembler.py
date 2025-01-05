19520180e85fe936297d5ca8a7aeec02378b7312
Ludovico Urbani <lu.urbani@outlook.com>
2025-01-05 14:11:23 +0100

    Assembler get labels

--- Python-Project/assembler.py
+++ Python-Project/assembler.py
@@ -1,12 +1,15 @@
 class Assembler(object):
+    _commands = ["ADD", "BRA", "BRP", "BRZ", "DAT", "HLT", "INP", "LDA", "OUT", "STA", "STO", "SUB"]
+
     def __init__(self, file_path: str, verbose: bool = False):
-        self.verbose = verbose
+        self._verbose = verbose
 
         self.success = None
 
         self.lmc_memory = [0] * 100
+        self._available_addresses = {}
 
-        if self.verbose:
+        if self._verbose:
             print("Loading file...")
         try:
             file = open(file_path, "r")
@@ -15,3 +18,33 @@ class Assembler(object):
             raise error
 
         self.assembly = file.read().split("\n")
+        if self._verbose:
+            print(f"Read {len(self.assembly)} lines")
+
+        self._compile()
+
+    def _compile(self):
+        self._get_labels()
+
+    def _get_labels(self):
+        if self._verbose:
+            print(f"Extracting labels...")
+
+        for cell, line in enumerate(self.assembly):
+            parts = get_line_parts(line)
+
+            if len(parts):
+                if parts[0] not in self._commands:
+                    self._available_addresses[parts[0]] = cell
+
+
+def get_line_parts(line):
+    line = remove_line_comments(line)
+    return line.upper().split()
+
+
+def remove_line_comments(line):
+    comment_index = line.find("//")
+    if comment_index >= 0:
+        return line[:comment_index]
+    return line

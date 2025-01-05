1ae28b845b240766637f86a4de57f87e2eb46eab
Ludovico Urbani <lu.urbani@outlook.com>
2025-01-05 13:42:15 +0100

    Assembler tester

---
+++ Python-Project/test_assembler.py
@@ -0,0 +1,12 @@
+from sys import argv
+
+from assembler import Assembler
+
+
+try:
+    argv[1]
+except IndexError:
+    print("Missing file path")
+    exit(1)
+
+ass = Assembler(argv[1], verbose = True)
